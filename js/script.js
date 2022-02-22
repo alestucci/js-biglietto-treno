87f8589c8dcbc307573f1f321f89c16bbc75b3a8
alestucci <alessandrotucci@gmail.com>
2022-02-22 01:24:26 +0100

    italoo finished

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 let userName = prompt('Come ti chiami?');
 let userAge = prompt('Quanti anni hai?');
 let ticketDistance = prompt('Quanti km devi percorrere?');
-
-document.getElementById('user-name').innerHTML = userName;
-
-let ticketPrice = (ticketDistance * 0.21).toFixed(2);
+let discount = 0
 
 if (userAge < 18) {
-    ticketPrice = (ticketPrice * 0.8).toFixed(2);
+    discount = 20;
 } else if (userAge > 65) {
-    ticketPrice = (ticketPrice * 0.6).toFixed(2);
+    discount = 40;
 } else {}
 
+let ticketPrice = (0.21 * ticketDistance * (1 - (discount / 100))).toFixed(2);
+
+document.getElementById('user-name').innerHTML = userName;
 document.getElementById('ticket-price').innerHTML = ticketPrice;
+document.getElementById('age').innerHTML = userAge;
+document.getElementById('distance').innerHTML = ticketDistance;
+document.getElementById('discount').innerHTML = discount;
