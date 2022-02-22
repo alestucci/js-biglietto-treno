926625349973c9280e4daa98d908006a4d4ed158
alestucci <alessandrotucci@gmail.com>
2022-02-22 14:07:53 +0100

    Constants, number control and error message added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
+const priceKm = 0.21;
+const youngDiscount = 20;
+const oldDiscount = 40;
+
 let userName = prompt('Come ti chiami?');
 let userAge = prompt('Quanti anni hai?');
 let ticketDistance = prompt('Quanti km devi percorrere?');
-let discount = 0
 
-if (userAge < 18) {
-    discount = 20;
-} else if (userAge > 65) {
-    discount = 40;
-} else {}
+userAge = parseFloat(userAge);
+ticketDistance = parseInt(ticketDistance);
+let discount = 0;
 
-let ticketPrice = (0.21 * ticketDistance * (1 - (discount / 100))).toFixed(2);
+if (isNaN(userAge) || isNaN(ticketDistance)) {
+    alert('Valore errato, si prega di inserire un numero.');
+    document.getElementById('price').innerHTML = 'Errore nell\'inserimento dei dati';
+} else if (userAge < 18) {
+        discount = youngDiscount;
+        let ticketPrice = (priceKm * ticketDistance * (1 - (discount / 100))).toFixed(2);
+        } else if (userAge >= 65) {
+            discount = oldDiscount;
+            let ticketPrice = (priceKm * ticketDistance * (1 - (discount / 100))).toFixed(2);
+                }
+
 
 document.getElementById('user-name').innerHTML = userName;
 document.getElementById('ticket-price').innerHTML = ticketPrice;
